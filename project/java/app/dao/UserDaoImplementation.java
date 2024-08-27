f399a7dfaa314dcef46d5108d0eb227f737f6ef7
SamuelRF17 <samuel.restrepo14@correo.tdea.edu.co>
2024-08-26 22:56:12 -0500

    conectar a BD

--- project/java/app/dao/UserDaoImplementation.java
+++ project/java/app/dao/UserDaoImplementation.java
@@ -1,9 +1,19 @@
 package project.java.app.dao;
 
 import project.java.app.dao.interfaces.UserDao;
+import project.java.app.DTO.UserDto;
 
 public class UserDaoImplementation implements UserDao {  
-    public UserDaoImplementation() {
+
+    public UserDto findByUserName(UserDto userDto) throws Exception {
+        return null; 
     }
- 
+    
+    public boolean existsByUserName(UserDto userDto) throws Exception {
+        return false; 
+    }
+    
+    public void createUser(UserDto userDto) throws Exception {
+    
+    }
 }
